3bdc075d22e91087fb9f7552f764ed79c2f8686e
Arya Venkatesan <ar6165@gmail.com>
2024-09-29 13:09:45 -0400

    Added button functionality and the color gradient

--- SpotifyClone/SpotifyClone/ContentView.swift
+++ SpotifyClone/SpotifyClone/ContentView.swift
@@ -12,11 +12,21 @@ struct ContentView: View {
     //var totalSongTime: Double = 97
     //var songTimeMinutes: Int
     //var songTimeSecond: Int
+    var lightMode: Bool = true;
+    //var textColor;
+    @State var likeCount: Int = 0;
+    @State var likeClicked: Bool = false;
+    @State var bookmarkClicked: Bool = false;
     var body: some View {
+//        if lightMode {
+//            textColor = Color(.white);
+//        } else {
+//            textColor = Color(.black);
+//        }
         let startColor = Color(red: 28.0/255, green: 39.0/255, blue: 46.0/255);
         let endColor = Color(red: 22.0/255, green: 26.0/255, blue: 29.0/255);
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [startColor, endColor]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [startColor, endColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
             .ignoresSafeArea()
                         
             VStack {
@@ -24,13 +34,16 @@ struct ContentView: View {
                     Image(systemName: "chevron.down")
                         .imageScale(.large)
                         .padding(.horizontal, 20)
+                        .foregroundColor(.white)
                     Spacer()
-                    Text("Go off again")
+                    Text("go off again")
                         .font(.caption)
+                        .foregroundColor(.white)
                     Spacer()
                     Image(systemName: "ellipsis")
                         .imageScale(.large)
                         .padding(.horizontal, 20)
+                        .foregroundColor(.white)
                 }
                 
                 VStack {
@@ -46,16 +59,52 @@ struct ContentView: View {
                     Spacer()
                     HStack {
                         VStack (alignment: .leading){
-                            Text("Go off again")
+                            Text("go off again")
                                 .font(.title3)
                                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                .foregroundColor(.white)
                             Text("sagun")
                                 .font(.caption)
+                                .foregroundColor(.white)
                         }
                         .padding(.horizontal, 20)
                         Spacer()
-                        Image(systemName: "heart")
-                            .padding(.horizontal, 20)
+                        Button { // Bookmark
+                            bookmarkClicked = !bookmarkClicked;
+                        } label: {
+                            if(bookmarkClicked){
+                                Image(systemName: "bookmark.fill")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                            } else {
+                                Image(systemName: "bookmark")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                            }
+                                
+                        }
+                        Button { // Heart
+                            likeCount+=1;
+                            likeClicked = !likeClicked;
+                            if(likeClicked){
+                                likeCount += 1;
+                            } else {
+                                likeCount -= 1;
+                            }
+                        } label: {
+                            if(likeClicked){
+                                Image(systemName: "heart.fill")
+                                    .font(.title)
+                                    .padding(.horizontal, 20)
+                                    .foregroundColor(.white)
+                            } else {
+                                Image(systemName: "heart")
+                                    .font(.title)
+                                    .padding(.horizontal, 20)
+                                    .foregroundColor(.white)
+                            }
+                                
+                        }
                         
                     }
                     .padding(.vertical, 20)
@@ -89,36 +138,44 @@ struct ContentView: View {
                         Image(systemName: "shuffle")
                             .font(.title2)
                             .padding(.horizontal, 16)
+                            .foregroundColor(.white)
                         HStack {
                             Image(systemName: "backward.end.fill")
                                 .font(.title2)
                                 .padding(.horizontal, 16)
+                                .foregroundColor(.white)
                         }
                         Image(systemName: "pause.circle.fill")
-                            .font(.largeTitle)
+                            .font(.system(size: 50))
                             .padding(.horizontal, 16)
+                            .foregroundColor(.white)
                         Image(systemName: "forward.end.fill")
                             .font(.title2)
                             .padding(.horizontal, 16)
+                            .foregroundColor(.white)
                         Image(systemName: "arrow.trianglehead.rectanglepath")
                             .renderingMode(.original)
                             .font(.title2)
                             .rotationEffect(.degrees(180))
                             .padding(.horizontal, 20)
+                            .foregroundColor(.white)
                     }
                     Spacer()
                     HStack {
                         VStack {
                             Image(systemName: "hifispeaker.2")
                                 .padding(.horizontal, 20)
+                                .foregroundColor(.white)
                         }
                         Spacer()
                         HStack{
                             Image(systemName: "square.and.arrow.up")
                                 .padding(.horizontal, 20)
+                                .foregroundColor(.white)
                             Image(systemName: "distribute.horizontal")
                                 .renderingMode(.original)
                                 .padding(.horizontal, 20)
+                                .foregroundColor(.white)
                         }
                     }
                     .padding(.vertical, 16)
